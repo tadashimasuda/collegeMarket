82e7c7d87518054754e7f8b1681b3c7676186ee8
tadashimasuda <tmasu4260@gmail.com>
2020-05-04 13:10:00 +0900

    html make form

--- resources/views/search_items.blade.php
+++ resources/views/search_items.blade.php
@@ -6,7 +6,17 @@
     @section('header')
 
     @endsection
+
     @section('main')
+    <div class="wrapper">
         <h2 id="search_title">検索結果</h2>
+
+        <form action="#" method="get" id="select_form">
+            <select name="" id="order">
+            
+            </select>
+        </form>
+
+    </div>
 
     @endsection
