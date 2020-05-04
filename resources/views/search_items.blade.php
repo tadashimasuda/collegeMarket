ee8cd3574138ea366ef94aeadda98a86cd50cc10
tadashimasuda <tmasu4260@gmail.com>
2020-05-04 13:16:20 +0900

    html make items

--- resources/views/search_items.blade.php
+++ resources/views/search_items.blade.php
@@ -1,22 +1,136 @@
 @extends('layouts.contents')
 @section('head')
-    @section('title','検索結果')
+@section('title','検索結果')
 @endsection
 
-    @section('header')
+@section('header')
 
-    @endsection
+@endsection
 
-    @section('main')
-    <div class="wrapper">
-        <h2 id="search_title">検索結果</h2>
+@section('main')
+<div class="wrapper">
+    <h2 id="search_title">検索結果</h2>
 
-        <form action="#" method="get" id="select_form">
-            <select name="" id="order">
-            
-            </select>
-        </form>
+    <form action="#" method="get" id="select_form">
+        <select name="" id="order">
 
+        </select>
+    </form>
+    <div id="search_items">
+        <ul class="main_items">
+            <li class="item">
+                <div class="item_img">
+                    <img src="" alt="">
+                </div>
+                <div class="item_info">
+                    <a href="#" class="item_title">タイトルタイトル</a>
+                    <div class="item_like">
+
+                    </div>
+                </div>
+            </li>
+            <li class="item">
+                <div class="item_img">
+                    <img src="" alt="">
+                </div>
+                <div class="item_info">
+                    <a href="#" class="item_title">タイトルタイトル</a>
+                    <div class="item_like">
+
+                    </div>
+                </div>
+            </li>
+            <li class="item">
+                <div class="item_img">
+                    <img src="" alt="">
+                </div>
+                <div class="item_info">
+                    <a href="#" class="item_title">タイトルタイトル</a>
+                    <div class="item_like">
+
+                    </div>
+                </div>
+            </li>
+            <li class="item">
+                <div class="item_img">
+                    <img src="" alt="">
+                </div>
+                <div class="item_info">
+                    <a href="#" class="item_title">タイトルタイトル</a>
+                    <div class="item_like">
+
+                    </div>
+                </div>
+            </li>
+            <li class="item">
+                <div class="item_img">
+                    <img src="" alt="">
+                </div>
+                <div class="item_info">
+                    <a href="#" class="item_title">タイトルタイトル</a>
+                    <div class="item_like">
+
+                    </div>
+                </div>
+            </li>
+            <div class="clear_fix"></div>
+            <li class="item">
+                <div class="item_img">
+                    <img src="" alt="">
+                </div>
+                <div class="item_info">
+                    <a href="#" class="item_title">タイトルタイトル</a>
+                    <div class="item_like">
+
+                    </div>
+                </div>
+            </li>
+            <li class="item">
+                <div class="item_img">
+                    <img src="" alt="">
+                </div>
+                <div class="item_info">
+                    <a href="#" class="item_title">タイトルタイトル</a>
+                    <div class="item_like">
+
+                    </div>
+                </div>
+            </li>
+            <li class="item">
+                <div class="item_img">
+                    <img src="" alt="">
+                </div>
+                <div class="item_info">
+                    <a href="#" class="item_title">タイトルタイトル</a>
+                    <div class="item_like">
+
+                    </div>
+                </div>
+            </li>
+            <li class="item">
+                <div class="item_img">
+                    <img src="" alt="">
+                </div>
+                <div class="item_info">
+                    <a href="#" class="item_title">タイトルタイトル</a>
+                    <div class="item_like">
+
+                    </div>
+                </div>
+            </li>
+            <li class="item">
+                <div class="item_img">
+                    <img src="" alt="">
+                </div>
+                <div class="item_info">
+                    <a href="#" class="item_title">タイトルタイトル</a>
+                    <div class="item_like">
+                    </div>
+                </div>
+            </li>
+            <div class="clear_fix"></div>
+        </ul>
     </div>
+</div>
 
-    @endsection
+@endsection
