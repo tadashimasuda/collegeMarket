b3ab2a651fd8031aceeb07da7a1b4d60af6cfd47
tadashimasuda <tmasu4260@gmail.com>
2020-05-06 21:09:44 +0900

    make user_img validator

--- app/Item.php
+++ app/Item.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    //keyword like search
     public function scopeNameLike($query,$str){
        return $query->where('title','like',"%{$str}%");
     }
