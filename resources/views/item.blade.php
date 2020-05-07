a5777d24b0f11aace746f050590288d9d39c410a
tadashimasuda <tmasu4260@gmail.com>
2020-05-07 15:54:21 +0900

    make blade item

---
+++ resources/views/item.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.contents')
+@section('head')
+@section('title','商品詳細')
+@endsection
+
+@section('header')
+
+@endsection
+
+@section('main')
+<div class="wrapper">
+    
+</div>
+
+@endsection
